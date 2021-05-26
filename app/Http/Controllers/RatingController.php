e29ee8c0dccb9cfe17cd18124e7502f5ad12b284
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2021-05-26 15:00:30 +0300

    REST API with Laravel API resources

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\RatingResource;
+use App\Models\Book;
+use App\Models\Rating;
+use Illuminate\Http\Request;
+
+class RatingController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function store(Request $request, Book $book)
+    {
+        $rating = Rating::firstOrCreate(
+            [
+                'user_id' => $request->user()->id,
+                'book_id' => $book->id,
+            ],
+            ['rating' => $request->rating]
+        );
+
+        return new RatingResource($rating);
+    }
+}
